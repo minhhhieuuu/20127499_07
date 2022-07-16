35fd15cebf4f9cf9f72c05af66835adc07f57453
minhhhieuuu <144020201hv@gmail.com>
2022-07-16 17:04:02 +0700

    lan2

---
+++ src/data.js
@@ -0,0 +1,95 @@
+import { tee_bad_habits_tobacco,orange_jacket,grey_tshirt,be_kind_tshirt ,street_gang_jacket,grey_hoodie,uni_cardigan,turquoise_sweater,basic_turquoise_tshirt,basic_blue_tshirt,misout_sweeter,black_sweater,bomber_jacket,black_white_jacket,grey_hoodi,drew_hoodie,mlb_cardigan,knit_cardigan } from "./picture/img";
+export const sliderItems = [
+    {
+      id: 1,
+      img: tee_bad_habits_tobacco,
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: orange_jacket,
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: grey_tshirt,
+      title: "POPULAR PRODUCE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: street_gang_jacket,
+      title: "JACKET STYLE!",
+    },
+    {
+      id: 2,
+      img: grey_hoodie,
+      title: "HOODIE STYLE!",
+    },
+    {
+      id: 3,
+      img: uni_cardigan,
+      title: "CARDIGAN STYLE!",
+    },
+    {
+        id: 4,
+        img: be_kind_tshirt,
+        title: "TSHIRT STYLE!",
+    },
+    {
+        id: 5,
+        img: turquoise_sweater,
+        title: "SWEATER STYLE!",
+    },
+  ];
+  export const popularProducts = [
+    {
+        id:1,
+        img:basic_turquoise_tshirt,
+    },
+    {
+        id:2,
+        img:basic_blue_tshirt,
+    },
+    {
+        id:3,
+        img:misout_sweeter,
+    },
+    {
+        id:4,
+        img:black_sweater,
+    },
+    {
+        id:5,
+        img:bomber_jacket,
+    },
+    {
+        id:6,
+        img:black_white_jacket,
+    },
+    {
+        id:7,
+        img:grey_hoodi,
+    },
+    {
+        id:8,
+        img:drew_hoodie,
+    },
+    {
+        id:9,
+        img:mlb_cardigan,
+    },
+    {
+        id:10,
+        img:knit_cardigan,
+    },
+  ]
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
 import NavBar from '../components/NavBar'
+import Products from '../components/Products'
+import Silder from '../components/Silder'
 
 const Home = () => {
   return (
     <div>
         <Announcement/>
         <NavBar/>
+        <Silder/>
+        <Categories/>
+        <Products/>
     </div>
   )
 }
